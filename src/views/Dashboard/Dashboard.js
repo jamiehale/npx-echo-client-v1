30571432da260645f824194b85486abeb553274e
Jamie Hale <jamiesonhale@gmail.com>
2020-02-19 13:55:41 -0500

    Remove reports from dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Switch, Route } from 'react-router-dom';
-import Menu from './Menu';
-import SelectReportType from './SelectReportType';
-import SystemHealthReportFlow from './SystemHealthReportFlow';
 
 import styles from "./dashboardStyle";
 
@@ -13,16 +9,6 @@ export default function Dashboard() {
   const classes = useStyles();
 
   return (
-    <Switch>
-      <Route exact path="/admin/dashboard">
-        <Menu classes={classes} />
-      </Route>
-      <Route exact path="/admin/dashboard/reports/new">
-        <SelectReportType classes={classes} />
-      </Route>
-      <Route path="/admin/dashboard/reports/new/system-health-report">
-        <SystemHealthReportFlow />
-      </Route>
-    </Switch>
+    <h2>Howdy</h2>
   );
 }
