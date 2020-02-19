25e7a6d1b7949ff2c255157b4a65115e5e5135e0
Jamie Hale <jamiesonhale@gmail.com>
2020-02-19 14:18:44 -0500

    Add actions table to dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,14 +1,50 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import GridItem from "../../components/Grid/GridItem.js";
+import GridContainer from "../../components/Grid/GridContainer.js";
+import Table from "../../components/Table/Table.js";
+import Card from "../../components/Card/Card.js";
+import CardHeader from "../../components/Card/CardHeader.js";
+import CardBody from "../../components/Card/CardBody.js";
 
 import styles from "./dashboardStyle";
 
 const useStyles = makeStyles(styles);
 
+const data = [
+  ['System', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Component', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Request', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['System', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Component', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Request', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['System', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Component', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+  ['Request', '2020-01-01', 'SCHMOE, Joe', '2020-01-02', '9', 'Critical'],
+];
+
 export default function Dashboard() {
   const classes = useStyles();
 
   return (
-    <h2>Howdy</h2>
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Actions</h4>
+          </CardHeader>
+          <CardBody style={{
+          maxHeight: '220px',
+          overflowY: 'scroll',
+        }}>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={['Type', 'Date Received', 'By Whom', 'Date Due', 'Priority', 'Labelling Section']}
+              tableData={data}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
   );
 }
