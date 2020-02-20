8b29dcc701e9c54861df51073a9d181b348526c7
Jamie Hale <jamiesonhale@gmail.com>
2020-02-20 15:12:36 -0500

    Add dashboard section titles

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
 import WarningIcon from '@material-ui/icons/Warning';
 import GridItem from "../../components/Grid/GridItem.js";
 import GridContainer from "../../components/Grid/GridContainer.js";
@@ -13,6 +14,12 @@ const ReponsibleSystemsIcon = () => (
 const ReportsCompletedIcon = () => (
   <i className="fas fa-user-check"></i>
 );
+
+const DashboardSectionTitle = styled.h2`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 300;
+`;
 
 const Dashboard = () => {
   const [isActionsExpanded, setActionsExpanded] = useState(false);
@@ -32,11 +39,17 @@ const Dashboard = () => {
         </GridItem>
       ) : (
         <>
+          <GridItem xs={12}>
+            <DashboardSectionTitle>To Do</DashboardSectionTitle>
+          </GridItem>
           <GridItem xs={12} sm={12} md={12}>
             <ActionsTable
               isExpanded={isActionsExpanded}
               onToggleExpand={handleToggleActionsExpanded}
             />
+          </GridItem>
+          <GridItem xs={12}>
+            <DashboardSectionTitle>KPIs</DashboardSectionTitle>
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <KpiCard
@@ -64,6 +77,9 @@ const Dashboard = () => {
               value={5}
               lastUpdated="yesterday"
             />
+          </GridItem>
+          <GridItem xs={12}>
+            <DashboardSectionTitle>Task Overview</DashboardSectionTitle>
           </GridItem>
           <GridItem xs={12} sm={12} md={6}>
             <SystemHealthPieCard />
